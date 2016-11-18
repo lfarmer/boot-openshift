fb895fdcb35f9d998c4537f20db5ae72b41eb893
leafarmer <leafdevelop@gmail.com>
2016-11-18 14:47:19 +0000

    First commit - adding spring boot admin for deployment to openshift

---
+++ src/main/java/openshift/Application.java
@@ -0,0 +1,23 @@
+package openshift;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
+
